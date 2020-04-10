c497983fadc8a09c7a067820bdd1b26ac5608e49
julia-francais <ju.francais@gmail.com>
2020-04-10 20:04:18 +0200

    design recipe details page

---
+++ src/components/DietGraph.js
@@ -0,0 +1,35 @@
+import * as React from "react";
+
+const DietGraph = ({ digest }: any) => {
+  digest.length = 3;
+
+  let sum: number = digest.reduce(
+    (sum: number, { total }: any) => sum + total,
+    0
+  );
+
+  return (
+    <ul className="dietGraph">
+      {digest.map(
+        ({ total, unit, daily, label, schemaOrgTag }: any, index: number) => (
+          <li
+            key={schemaOrgTag}
+            className={`dietGraph__item dietGraph__item--${index}`}
+          >
+            <div
+              className={`dietGraph__graph dietGraph__graph--${index}`}
+              style={{ height: `${(total / sum) * 150}px` }}
+            />
+            <small className="dietGraph__label">{label}</small>
+
+            <small className="dietGraph__info">
+              {`${total.toFixed(2)}${unit} (${daily.toFixed(2)}%)`}
+            </small>
+          </li>
+        )
+      )}
+    </ul>
+  );
+};
+
+export default DietGraph;
--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import DietGraph from "./DietGraph";
+
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Badge from "react-bootstrap/Badge";
@@ -12,68 +14,83 @@ export const RecipeDetails = (props) => {
   const recipe = props.recipe[0].recipe;
 
   return (
-    <Container>
+    <Container className="mt-4">
+      <h1 className="display-3 my-4">{recipe.label}</h1>
       <Row>
-
-    <Col sm="3" key={recipe.uri}>
-      <img src={recipe.image} alt="Dish" width="100" />
-      <p>{recipe.label}</p>
-      <Col sm="3">
-        <h2>Ingredients :</h2>
-        <ul>
-          {recipe.ingredientLines.map((ingredient, index) => (
-            <li key={index}>{ingredient}</li>
-          ))}
-        </ul>
-      </Col>
-      <Col sm="3">
-        <h2>Preparations</h2>
-        <p>
-          Recipe provided by : <a href={recipe.url}>{recipe.source}</a>
-        </p>
-        <p>
-          View the detailed preparation instructions by clicking the following
-          link :
-        </p>
-        <a href={recipe.url} target="_blank">
-          <Button color="info">View the preparation</Button>
-        </a>
-      </Col>
+        <Col sm="4" key={recipe.uri}>
+          <img src={recipe.image} alt="Dish" className="recipeDetailsImage" />
+          <Row>
+            <Col sm="6">{Math.round(recipe.calories)} calories</Col>
+            <Col sm="6">{recipe.yield} servings</Col>
+          </Row>
+        </Col>
+        <Col sm="4">
+          <h2>Ingredients :</h2>
+          <ul>
+            {recipe.ingredientLines.map((ingredient, index) => (
+              <li key={index}>{ingredient}</li>
+            ))}
+          </ul>
+        </Col>
+        <Col sm="4">
+          <h2>Preparations</h2>
+          <p>
+            Recipe provided by : <a href={recipe.url}>{recipe.source}</a>
+          </p>
+          <p>
+            View the detailed preparation instructions by clicking the following
+            link :
+          </p>
+          <a href={recipe.url} target="_blank">
+            <Button color="info">View the preparation</Button>
+          </a>
+        </Col>
       </Row>
-
-      <div>
-        <p>
-          Diet Labels:{" "}
-          {recipe.dietLabels.length ? (
-            recipe.dietLabels.map((label, index) => (
-              <h5 key={index}>
-                <Badge variant="success" pill>
-                  {label} {index !== recipe.dietLabels.length - 1 && "| "}
-                </Badge>
-              </h5>
-            ))
-          ) : (
-            <span>No health label attributed yet.</span>
-          )}
-        </p>
-        <p>
-          Health Labels:{" "}
-          {recipe.healthLabels.length ? (
-            recipe.healthLabels.map((label, index) => (
-              <h4 key={index}>
-                <Badge pill variant="info">
-                  {label} {index !== recipe.healthLabels.length - 1 && "| "}
-                </Badge>
-              </h4>
-            ))
-          ) : (
-            <span>No health label attributed yet.</span>
-          )}
-        </p>
-      </div>
-      <Link to="/">Go back to other delicious recipes</Link>
+      <Row className="mt-4">
+        <Col sm="6">
+          <Row>
+            <Col sm="4">Diet Labels:</Col>
+            <Col sm="8">
+              {recipe.dietLabels.length ? (
+                recipe.dietLabels.map((label, index) => (
+                  <h5 key={index}>
+                    <Badge pill variant="info">
+                      {label} {index !== recipe.dietLabels.length - 1 && "| "}
+                    </Badge>
+                  </h5>
+                ))
+              ) : (
+                <span>No health label attributed yet.</span>
+              )}
+              <DietGraph digest={recipe.digest} />
+            </Col>
+          </Row>
+        </Col>
+        <Col sm="6">
+          <Row>
+            <Col sm="4">Health Labels:</Col>
+            <Col sm="8">
+              {recipe.healthLabels.length ? (
+                recipe.healthLabels.map((label, index) => (
+                  <h4 key={index}>
+                    <Badge pill variant="success">
+                      {label}
+                    </Badge>
+                  </h4>
+                ))
+              ) : (
+                <span>No health label attributed yet.</span>
+              )}
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <Link to="/">
+        <Button className="linkButton">
+          Go back to other delicious recipes
+        </Button>
+      </Link>
     </Container>
-
   );
 };
 
